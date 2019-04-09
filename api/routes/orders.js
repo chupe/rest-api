1a382a0726fc6971936881ed0f75e4a6ce812eae
chupe <velahavle@gmail.com>
2019-04-09 08:59:34 +0200

    Starting

---
+++ api/routes/orders.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling get requests to /orders'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created'
+    })
+})
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'order details',
+        orderId: req.params.orderId
+    })
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'order deleted'
+    })
+})
+
+module.exports = router
---
+++ api/routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling get requests to /products'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling post requests to /products'
+    })
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            ID: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an id'
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'updated product'
+    })
+
+})
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'deleted product'
+    })
+
+})
+
+module.exports = router
